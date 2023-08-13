b0931477407ba4c673cb45e0987136e9fe46d9e1
Aleksandr Szestak <szestak@hotmail.com>
2023-08-13 22:53:39 +0300

    fix: change in header section

--- library/index.js
+++ library/index.js
@@ -1,6 +1,15 @@
+const burger = document.querySelector(".burger");
+
+burger.addEventListener('click', ()=>{
+    document.querySelector('header').classList.toggle('menu-open')
+});
+
+
+/*
 console.log('Вёрстка:');
 console.log('1) валидная +10');
 console.log('2) семантическая +16');
 console.log('3) соответствие макету +50');
 console.log('4) общие требования к макету +20');
 console.log('итог - 96');
+*/
