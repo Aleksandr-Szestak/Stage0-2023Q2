de955619dc9d3c2a9dd127162685c09a7e33f3bf
aleksandr-szestak <szestak@hotmail.com>
2023-08-25 16:52:13 +0300

    fix: relayout About section

--- library/index.js
+++ library/index.js
@@ -1,12 +1,33 @@
-const burger = document.querySelector(".burger");
+let burger = document.querySelector(".burger");
 
 burger.addEventListener('click', ()=>{
     document.querySelector('ul').classList.toggle('burger-menu-open');
     document.querySelector('.burger').classList.toggle('burger-open');
 });
 
+// Получаем slide1, slide2, slide3
+let btnSlide1 = document.getElementById("slide1");
+let btnSlide2 = document.getElementById("slide2");
+let btnSlide3 = document.getElementById("slide3");
+
+if (btnSlide1.checked) {
+      console.log('Checkbox1 is checked');
+    } 
+else if (btnSlide2.checked){
+      console.log('Checkbox2 is not checked');
+    }
+else if (btnSlide3.checked){
+      console.log('Checkbox3 is not checked');
+    }   
+
+
+
+
+
+/*
 console.log('Вёрстка:');
 console.log('1) соответствует макету 24/26');
 console.log('2) не появл.полоса прокрутки 12/12');
 console.log('3) бургер-меню 0/12');
 console.log('всего баллов - 36/50');
+*/
