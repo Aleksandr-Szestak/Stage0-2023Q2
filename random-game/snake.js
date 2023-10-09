b11cbb0ea126fffa1d6b64490a88b3526a944dfe
Aleksandr Szestak <szestak@hotmail.com>
2023-10-09 22:57:14 +0300

    feat: continuation#3 of task implementation

--- random-game/snake.js
+++ random-game/snake.js
@@ -20,9 +20,8 @@ ctx.beginPath();
 //горизонтальная линия
 // ctx.moveTo(10, 50);  
 // ctx.lineTo(500, 50); //вправо
-
-
-console.log(amountX);
+//
+//console.log(amountX);
 for(i=0; i<=amountX; i++){
     ctx.moveTo(stepGrid * (i+1), gapBorder);  
     ctx.lineTo(stepGrid * (i+1), canvas.height - gapBorder);
@@ -35,7 +34,47 @@ for(i=0; i<=amountY; i++){
 
 ctx.stroke();
 //------------------------------------------------
-const star = new Image();
-star.src = '/assets/star.png';
+const starImg = new Image();
+starImg.src = './assets/star.png';
+
+console.log(starImg);
+
+let star = {
+	x: Math.floor((Math.random() * amountX)) * stepGrid,
+	y: Math.floor((Math.random() * amountY)) * stepGrid,
+};
+
+let snake = [];
+snake[0] = {
+	x: Math.trunc(amountX / 2) * stepGrid,
+	y: Math.trunc(amountY / 2) * stepGrid
+};
+
+//------------------------------------------------
+let gameProcess = setInterval(gameSnake,300);
 
 
+
+
+//------------------------------------------------
+function gameSnake(){
+    // let i;
+    // for(i=0; i<=amountX; i++){
+    //     ctx.moveTo(stepGrid * (i+1), gapBorder);  
+    //     ctx.lineTo(stepGrid * (i+1), canvas.height - gapBorder);
+    // }
+    
+    // for(i=0; i<=amountY; i++){
+    //     ctx.moveTo(gapBorder, stepGrid * (i+1));  
+    //     ctx.lineTo(canvas.width - gapBorder, stepGrid * (i+1));
+    // }
+
+    ctx.drawImage(starImg, star.x, star.y);
+
+    for(let i = 0; i < snake.length; i++) {
+		// ctx.fillStyle = i == 0 ? "green" : "red";
+        ctx.fillStyle = "green";
+		ctx.fillRect(snake[i].x, snake[i].y, stepGrid, stepGrid);
+	}
+
+}
